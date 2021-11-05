41868d7b5e783f8c2a52e555ce221dc671741956
lipenathan <felipenatthan@gmail.com>
2021-11-05 17:40:02 -0300

    classe de Suspeito

---
+++ app/src/main/java/com/example/projeto_mulher/dominio/Suspeito.java
@@ -0,0 +1,17 @@
+package com.example.projeto_mulher.dominio;
+
+/**
+ * Representa domínio de suspeito
+ * @author Felipe Nathan
+ * @version 1.0 05/11/2021
+ * @since 05/11/2021
+ */
+public class Suspeito extends Pessoa{
+
+    /**
+     * método responsável por disparar alerme de proximidade do suspeito
+     */
+    public void monitorarLocalização() {
+        // falta implementar
+    }
+}
